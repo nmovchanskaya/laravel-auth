8f88b3108bfa440f8b910f78bbffeaa145dc13d0
nika.movchanskaya <nika.movchanskaya@gmail.com>
2024-04-24 13:14:49 +0200

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/userinfo', [App\Http\Controllers\UserInfoController::class, 'index']);
+});
